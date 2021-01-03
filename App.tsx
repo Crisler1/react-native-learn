6afe70ec9541e83925c0fde59d223c0a326cca7b
Crisler Wintler <crislerwintler@gmail.com>
2021-01-02 22:55:17 -0300

    update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import {  Text, View } from 'react-native'
+import {Button,  Text, View } from 'react-native'
 import styles from './styles/styleSheet'
 const App = () => {
+  const [state, setState] = React.useState(0)
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text>{state }</Text>
+      <Button
+  onPress={() => setState(state + 1) }
+  title="Learn More"
+  color="#841584"
+  />
     </View>
   )
 }
